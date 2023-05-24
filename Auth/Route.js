dfab79f8edc0ce77a72a1756500b2cd333cc7a26
engyyyyy <engymakram008@gmail.com>
2023-05-24 18:29:59 +0300

    engyyyyy edit

---
+++ Auth/Route.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const router = express.Router()
+const { register, login, update } = require("./Auth");
+router.route("/register").post(register)
+router.route("/login").post(login);
+router.route("/update").put(update);
+module.exports = router
