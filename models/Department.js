717490473f02c14ca2ae00e0b8abde06318ef934
Esmail Fawzy <esmail.elshishtawy.2001@gmail.com>
2023-05-24 19:50:03 +0300

    fix departments

--- models/Department.js
+++ models/Department.js
@@ -1,15 +1,21 @@
 import mongoose from "mongoose";
 
-const DepartmentSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const DepartmentSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    dep_code: {
+      type: String,
+      required: true,
+    },
   },
-  dep_code: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
 const Department = mongoose.model("Department", DepartmentSchema);
 
