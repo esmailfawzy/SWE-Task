f60b9a4c3620fe90438d0a5d09b50304e6760ca7
Your Name <you@example.com>
2023-05-25 01:33:09 +0300

    edit

--- Auth/Auth.js
+++ Auth/Auth.js
@@ -6,119 +6,8 @@ import { jwt } from "jsonwebtoken"
 const jwtSecret = 'f52560967c6259acfdb699422038fc983af277f0bb208c7fc389ea913674836a4b1d91'
 
 // auth.js
-exports.register = async (req, res, next) => {
+export const register = async (req, res, next) => {
     const { username, password } = req.body
-    if (password.length < 6) {
-      return res.status(400).json({ message: "Password less than 6 characters" })
-    }
-    try {
-      await User.create({
-        username,
-        password,
-      }).then(user =>
-        res.status(200).json({
-          message: "User successfully created",
-          user,
-        })
-      )
-    } catch (err) {
-      res.status(401).json({
-        message: "User not successful created",
-        error: error.mesage,
-      })
-    }
-  }
-
-  // auth.js
-exports.login = async (req, res, next) => {
-    const { username, password } = req.body
-    // Check if username and password is provided
-    if (!username || !password) {
-      return res.status(400).json({
-        message: "Username or Password not present",
-      })
-    }
-  }
-
-  exports.login = async (req, res, next) => {
-    try {
-      const user = await User.findOne({ username, password })
-      if (!user) {
-        res.status(401).json({
-          message: "Login not successful",
-          error: "User not found",
-        })
-      } else {
-        res.status(200).json({
-          message: "Login successful",
-          user,
-        })
-      }
-    } catch (error) {
-      res.status(400).json({
-        message: "An error occurred",
-        error: error.message,
-      })
-    }
-  }
-
-  //auth.js
-exports.update = async (req, res, next) => {
-    const { role, id } = req.body
-    // Verifying if role and id is presnt
-    if (role && id) {
-      // Verifying if the value of role is admin
-      if (role === "admin") {
-        await User.findById(id)
-      } else {
-        res.status(400).json({
-          message: "Role is not admin",
-        })
-      }
-    } else {
-      res.status(400).json({ message: "Role or Id not present" })
-    }
-  }
-
-  
-  exports.update = async (req, res, next) => {
-    const { role, id } = req.body;
-    // First - Verifying if role and id is presnt
-    if (role && id) {
-      // Second - Verifying if the value of role is admin
-      if (role === "admin") {
-        // Finds the user with the id
-        await User.findById(id)
-          .then((user) => {
-            // Third - Verifies the user is not an admin
-            if (user.role !== "admin") {
-              user.role = role;
-              user.save((err) => {
-                //Monogodb error checker
-                if (err) {
-                  res
-                    .status("400")
-                    .json({ message: "An error occurred", error: err.message });
-                  process.exit(1);
-                }
-                res.status("201").json({ message: "Update successful", user });
-              });
-            } else {
-              res.status(400).json({ message: "User is already an Admin" });
-            }
-          })
-          .catch((error) => {
-            res
-              .status(400)
-              .json({ message: "An error occurred", error: error.message });
-          });
-        }
-    }
-}
-
-exports.register = async (req, res, next) => {
-    const { username, password } = req.body;
-  
     bcrypt.hash(password, 10).then(async (hash) => {
       await User.create({
         username,
@@ -149,10 +38,13 @@ exports.register = async (req, res, next) => {
           })
         );
     });
-  };
 
+  }
 
-  exports.login = async (req, res, next) => {
+  
+  // auth.js
+
+  export const login = async (req, res, next) => {
     const { username, password } = req.body
     // Check if username and password is provided
     if (!username || !password) {
@@ -200,8 +92,45 @@ exports.register = async (req, res, next) => {
       }
     };
 
+  //auth.js
+  export const update = async (req, res, next) => {
+    const { role, id } = req.body;
+    // First - Verifying if role and id is presnt
+    if (role && id) {
+      // Second - Verifying if the value of role is admin
+      if (role === "admin") {
+        // Finds the user with the id
+        await User.findById(id)
+          .then((user) => {
+            // Third - Verifies the user is not an admin
+            if (user.role !== "admin") {
+              user.role = role;
+              user.save((err) => {
+                //Monogodb error checker
+                if (err) {
+                  res
+                    .status("400")
+                    .json({ message: "An error occurred", error: err.message });
+                  process.exit(1);
+                }
+                res.status("201").json({ message: "Update successful", user });
+              });
+            } else {
+              res.status(400).json({ message: "User is already an Admin" });
+            }
+          })
+          .catch((error) => {
+            res
+              .status(400)
+              .json({ message: "An error occurred", error: error.message });
+          });
+        }
+    }
+}
+  
 
-exports.adminAuth = (req, res, next) => {
+
+export const adminAuth = (req, res, next) => {
   const token = req.cookies.jwt
   if (token) {
     jwt.verify(token, jwtSecret, (err, decodedToken) => {
@@ -222,7 +151,7 @@ exports.adminAuth = (req, res, next) => {
   }
 }
 
-exports.userAuth = (req, res, next) => {
+export const userAuth = (req, res, next) => {
     const token = req.cookies.jwt
     if (token) {
       jwt.verify(token, jwtSecret, (err, decodedToken) => {
@@ -243,7 +172,7 @@ exports.userAuth = (req, res, next) => {
     }
   }
 
-  exports.getUsers = async (req, res, next) => {
+  export const getUsers = async (req, res, next) => {
     await User.find({})
       .then(users => {
         const userFunction = users.map(user => {
